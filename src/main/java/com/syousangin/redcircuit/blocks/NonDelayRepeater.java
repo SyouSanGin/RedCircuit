bcd88919d0c7ab083ccfee6c383c75f31f0d49ad
FanGao <agno3@mail.ustc.edu.cn>
2022-02-08 09:58:06 +0800

    Programming

--- src/main/java/com/syousangin/redcircuit/blocks/NonDelayRepeater.java
+++ src/main/java/com/syousangin/redcircuit/blocks/NonDelayRepeater.java
@@ -2,8 +2,7 @@ package com.syousangin.redcircuit.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-
-import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DiodeBlock;
 import net.minecraft.world.level.block.SoundType;
@@ -21,19 +20,22 @@ public class NonDelayRepeater extends DiodeBlock {
 
     public NonDelayRepeater() {
         super(BlockBehaviour.Properties.of(Material.DECORATION).instabreak().sound(SoundType.WOOD));
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(POWERED, Boolean.valueOf(false)));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(POWERED, false));
     }
 
-    protected boolean isAlternateInput(BlockState pState) {
+    protected boolean isAlternateInput(@NotNull BlockState pState) {
         return isDiode(pState);
     }
 
     @Override
-    protected int getDelay(BlockState pState) {
+    protected int getDelay(@NotNull BlockState pState) {
         return 0;
     }
 
-
+    @Override
+    public boolean canSurvive(@NotNull BlockState pState, @NotNull LevelReader pLevel, BlockPos pPos) {
+        return canSupportRigidBlock(pLevel,pPos.below());
+    }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING,POWERED);
